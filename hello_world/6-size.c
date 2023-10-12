ab06d5231a4a4045a977eac736370e28e01cb9c0
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-10-12 11:42:13 -0400

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf("Size of char: %zu bytes/n", sizeof(charType));
-	printf("Size of int: %zu bytes/n", sizeof(intType));
-	printf("Size of long int: %zu bytes/n", sizeof(longintType));
-	printf("Size of long long int: %zu bytes/n", sizeof(longlongintType));
-	printf("Size of float: %zu bytes/n", sizeof(floatType));
+	printf("Size of char: %zu byte(s)/n", sizeof(char));
+	printf("Size of int: %zu byte(s)/n", sizeof(int));
+	printf("Size of long int: %zu byte(s)/n", sizeof(long int));
+	printf("Size of long long int: %zu byte(s)/n", sizeof(long long int));
+	printf("Size of float: %zu byte(s)/n", sizeof(float));
 
 	return (0);
 }

a97505b08324f8632b9e1f17ce01d1d11fca02f5
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-10-19 10:14:47 -0400

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isidigit - Checks for a digit from 0 through 9
+ * @c: variable
+ * Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
+

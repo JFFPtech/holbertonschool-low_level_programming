fa970de74f276b745f918bd21116ca09221019fc
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-10-23 10:32:30 -0400

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take pointer to int parameter and updates value to 98
+ *@n: value of int
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}

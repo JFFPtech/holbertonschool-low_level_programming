0de08c5a457062233b9200433edc014fda644b2a
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-10-23 11:20:38 -0400

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the input string
+ *
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++);
+
+	return (index);
+}
+

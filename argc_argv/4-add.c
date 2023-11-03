6fc9dc665eda294ebbd0a94048b755a66b97556e
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-11-03 10:50:06 -0400

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+/**
+ * _isnumber - checks if string is a number
+ * @s: string
+ *
+ * Return: 1 on success
+ * if not return 0.
+ */
+
+int _isnumber(char *s)
+{
+	int i, check, d;
+
+	i = 0, d = 0, check = 1;
+	if (*s == '-')
+		i++;
+	for (; *(s + i) != 0; i++)
+	{
+		d = isdigit(*(s + i));
+		if (d == 0)
+		{
+			check = 0;
+			break;
+		}
+	}
+	return (check);
+}
+
+/**
+ * main - Entry point
+ *
+ * @argc: counts the number of parameters that go into main
+ * @argv: pointer of array containing strings entering main
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i, n, ex;
+
+	ex = 0, n = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (_isnumber(argv[i]))
+				n += atoi(argv[i]);
+			else
+				ex = 1;
+		}
+	}
+	if (ex == 0)
+		printf("%i\n", n);
+	else
+		printf("%s\n", "Error");
+	return (ex);
+}

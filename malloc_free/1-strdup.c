c113a2c952d99bcf6c4ee212c000f9c06ae16d7a
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-11-09 22:39:11 -0400

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string into a new array
+ * @str: string to duplicate
+ *
+ * Return: pointer to string
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *ptr, *ret;
+
+	if (!str)
+		return (NULL);
+
+	ptr = str;
+	while (*ptr++)
+		size++;
+
+	ret = malloc(size + 1);
+	if (!ret)
+		return (NULL);
+
+	ptr = ret;
+	while (*str)
+		*ptr++ = *str++;
+
+	*ptr = 0;
+	return (ret);
+}

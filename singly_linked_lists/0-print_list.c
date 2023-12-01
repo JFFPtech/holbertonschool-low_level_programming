253c96c99c9769b00fc8be90ea679302d325d1dd
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-12-01 10:35:21 -0400

    added main files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints everything in a list_t list
+ * @h: the list_t list
+ *
+ * Return: the number of nodes in list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (null)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}

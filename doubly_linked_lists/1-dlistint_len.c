07ce724d64610e78db68a8950b217d58f1752372
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-12-03 12:12:45 -0400

    1-dlistint_len.c

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - counts the number of elements in a linked list
+ * @h: the head of the list.
+ *
+ * Return: the count of what is in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}

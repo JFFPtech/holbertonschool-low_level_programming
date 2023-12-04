439decdaa058b7d94557d88b49338ba0b08023cf
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-12-03 20:56:44 -0400

    5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -3,14 +3,14 @@
 /**
  * flip_bits - counts the number of bits needed to be flipped
  * @n: the number of bits
- * @k: the number to flip n 
+ * @m: the number to flip n 
  *
  * Return: number of bits to flip to get from n to k
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ k, bits = 0;
+	unsigned long int xor = n ^ m, bits = 0;
 
 	while (xor > 0)
 	{
